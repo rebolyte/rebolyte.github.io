26092abd97f74bf66bc3808260b49ffccee5de8b
James Irwin <rebolyte@gmail.com>
2015-02-17 19:04:59 -0700

    Update gulpfile and index

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 var gulp	= require('gulp');
 var deploy	= require('gulp-gh-pages');
 
+// http://www.michael-whelan.net/deploying-sandra-snow-to-github-pages-with-gulp/
+
 gulp.task('build', function () {
   gulp.src('./src/**/*')
     .pipe(gulp.dest('./dist'));
 });
 
 var options = { 
-	remoteUrl: "https://github.com/mwhelan/rebolyte.github.io.git",
-	branch: "master"
+	remoteUrl: 'https://github.com/rebolyte/rebolyte.github.io.git',
+	branch: 'master'
 };
 /**
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
+  return gulp.src('./dist/**/*')
     .pipe(deploy(options))
 });
